cf077a0e910f9c15dd1e7e33c734dd00556bbe61
compwron <wordymusic@gmail.com>
2013-05-27 17:02:57 -0500

    made euler1 into a class so that it could have a method which would return and be testable

--- euler/euler1.py
+++ euler/euler1.py
@@ -2,9 +2,12 @@
 # are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
-result = 0
-for i in range(1, 1000):
-	if i % 3 == 0 or i % 5 == 0:
-		result += i
-print(result)
+class euler1:
+	def result():
+		current = 0
+		for i in range(1, 1000):
+			if i % 3 == 0 or i % 5 == 0:
+				current += i
+		return current
 
+	print(result())
--- tests/test_euler.py
+++ tests/test_euler.py
@@ -1,2 +1,5 @@
 def test_b():
     assert 'b' == 'b'
+
+def test_euler1():
+	assert 
